a9e9477428ab233f6c09585e5e7d4d339b01d75d
Minh Khoi <Minhkhoi01102022@outlook.com.vn>
2024-02-24 22:10:04 +0700

    test cmt

--- app/src/main/java/com/example/mangaapp/MainActivity.java
+++ app/src/main/java/com/example/mangaapp/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addControls();
+        //test cmt
     }
 
     private void addControls() {
