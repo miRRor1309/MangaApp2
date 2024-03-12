7c46b3a0f71670b1d5a5c93e447660eaa1544a94
Minh Khoi <Minhkhoi01102022@outlook.com.vn>
2024-03-12 21:21:43 +0700

    ráp Main_activity

--- app/src/main/java/com/example/mangaapp/LoginActivity.java
+++ app/src/main/java/com/example/mangaapp/LoginActivity.java
@@ -31,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         addControls();
         addEvents();
-
+        //test connect
     }
 
     private void addEvents() {
