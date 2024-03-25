43af29574edf3a90847581aed345b191c230db60
ZALDAL <lenguyenductai208@gmail.com>
2024-03-26 06:48:43 +0700

    thêm chức năng đổi màu app

--- app/src/main/java/com/example/mangaapp/UserInfoActivity.java
+++ app/src/main/java/com/example/mangaapp/UserInfoActivity.java
@@ -1,12 +1,16 @@
 package com.example.mangaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -20,11 +24,38 @@ public class UserInfoActivity extends AppCompatActivity {
     Button btnLogOut,btnReturn,btnChangePass;
     FirebaseAuth mAuth;
     FirebaseFirestore fStore;
+    Switch switcher;
+    boolean nightmode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        switcher = findViewById(R.id.switcher);
+
+        // Sử dụng chế độ SharedPreferences để lưu chế độ nếu thoát ứng dụng và quay lại
+        sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE);
+        nightmode = sharedPreferences.getBoolean("night", false); // Chế độ sáng là mặc định
+        if (nightmode) {
+            switcher.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        switcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nightmode = !nightmode; // Đảo ngược trạng thái của nightmode
+                if (nightmode) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+                editor = sharedPreferences.edit();
+                editor.putBoolean("night", nightmode);
+                editor.apply();
+            }
+        });
         addControls();
         btnChangePass.setOnClickListener(new View.OnClickListener() {
             @Override
