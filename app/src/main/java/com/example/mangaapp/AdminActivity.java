f826ed3d519e2c04c78ec091f84468256e062cdf
miRRor1309 <145328904+miRRor1309@users.noreply.github.com>
2024-03-26 07:58:05 +0700

    them mau chu

--- app/src/main/java/com/example/mangaapp/AdminActivity.java
+++ app/src/main/java/com/example/mangaapp/AdminActivity.java
@@ -1,19 +1,35 @@
 package com.example.mangaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.mangaapp._interface.IClickMangaListener;
+import com.example.mangaapp.adapter.MangaAdapter;
+import com.example.mangaapp.model.Manga;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AdminActivity extends AppCompatActivity {
@@ -21,6 +37,12 @@ public class AdminActivity extends AppCompatActivity {
     TextView txtUserEmail, txtUserName;
     FirebaseAuth mAuth;
     FirebaseFirestore fStore;
+
+    public static SQLiteDatabase database = null;
+    TextView txt_Username;
+    private RecyclerView recyclerViewManga;
+    private MangaAdapter mangaAdapter;
+    private SearchView searchView;
 
 
     @Override
@@ -80,5 +102,6 @@ public class AdminActivity extends AppCompatActivity {
         btnLogOut=findViewById(R.id.btnLogOut);
         btnAdd=findViewById(R.id.btnAdd);
     }
+
 
 }
